37732963750be833b28d8ee2d2a1adeb0d9400e6
azaroxlive <azaroxoriginel@gmail.com>
2025-05-20 13:58:02 +0200

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,18 +1,17 @@
 const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   const APP_ID = '9e8dc5a99fb240a8bd6513630c14a45c';
   const APP_CERTIFICATE = '0b1ec530eb22494486b4c7a6b234b18c';
-
   const channelName = req.query.channelName;
-  const uid = parseInt(req.query.uid) || 0;
+  const uid = parseInt(req.query.uid) || 1;
 
   if (!channelName) {
     return res.status(400).json({ error: 'channelName requis' });
   }
 
   const role = RtcRole.PUBLISHER;
-  const expireTime = 3600;
+  const expireTime = 86400;
   const currentTime = Math.floor(Date.now() / 1000);
   const privilegeExpiredTs = currentTime + expireTime;
 
