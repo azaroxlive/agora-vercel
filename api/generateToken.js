24a2ace79d47eb76844f6e030d3c79288a9486f7
azaroxlive <azaroxoriginel@gmail.com>
2025-05-23 21:43:10 +0200

    Update generateToken.js

--- api/generateToken.js
+++ api/generateToken.js
@@ -1,27 +1,30 @@
-const Agora = require('agora-access-token');
+const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 
 module.exports = (req, res) => {
-  const appId = process.env.9e8dc5a99fb240a8bd6513630c14a45c;
-  const appCertificate = process.env.0b1ec530eb22494486b4c7a6b234b18c;
+  const appId = process.env.AGORA_APP_ID;
+  const appCertificate = process.env.AGORA_APP_CERTIFICATE;
   const channelName = req.query.channelName;
   const uid = req.query.uid;
+
   const role = RtcRole.PUBLISHER;
   const expiration = 86400; // 24h
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const privilegeExpireTime = currentTimestamp + expiration;
 
-  if (!appId || !appCertificate || !channelName) {
+  if (!appId || !appCertificate || !channelName || !uid) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
-const token = Agora.RtcTokenBuilder.buildTokenWithAccount(
-  appId,
-  appCertificate,
-  channelName,
-  uid, // string comme 'uabc123'
-  role,
-  expiration
-);
+  const token = RtcTokenBuilder.buildTokenWithUid(
+    appId,
+    appCertificate,
+    channelName,
+    parseInt(uid),
+    role,
+    privilegeExpireTime
+  );
 
-  res.json({
+  return res.status(200).json({
     token: token,
     rtcUid: uid
   });
