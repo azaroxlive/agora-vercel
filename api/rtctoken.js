218af8418c7e2c84120d534e50690c60e45f8c06
azaroxlive <azaroxoriginel@gmail.com>
2025-05-23 21:55:57 +0200

    Update rtctoken.js

--- api/rtctoken.js
+++ api/rtctoken.js
@@ -1,32 +1,29 @@
-const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
-console.log("RTC TOKEN API CALLED");
+const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 
 module.exports = (req, res) => {
-  const appId = process.env.AGORA_APP_ID;
-  const appCertificate = process.env.AGORA_APP_CERTIFICATE;
+  const appId = process.env.APP_ID;
+  const appCertificate = process.env.APP_CERTIFICATE;
+
   const channelName = req.query.channelName;
   const uid = req.query.uid;
-
   const role = RtcRole.PUBLISHER;
   const expiration = 86400; // 24h
-  const currentTimestamp = Math.floor(Date.now() / 1000);
-  const privilegeExpireTime = currentTimestamp + expiration;
 
   if (!appId || !appCertificate || !channelName || !uid) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
-  const token = RtcTokenBuilder.buildTokenWithUid(
+  const token = RtcTokenBuilder.buildTokenWithAccount(
     appId,
     appCertificate,
     channelName,
-    parseInt(uid),
+    uid,
     role,
-    privilegeExpireTime
+    Math.floor(Date.now() / 1000) + expiration
   );
 
   return res.status(200).json({
-    token: token,
+    token,
     rtcUid: uid
   });
 };
